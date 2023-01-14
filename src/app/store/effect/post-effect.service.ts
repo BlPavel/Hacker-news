474b26538db8fb46dfcb6fc7d2de2a44b0d8db5e
Pavel Blokhin <BlokhinPavel92@yandex.ru>
2023-01-14 14:32:03 +0300

    create post-effect

--- src/app/store/effect/post-effect.service.ts
+++ src/app/store/effect/post-effect.service.ts
@@ -1,9 +1,24 @@
+import { IHitItem } from './../../shared/interfaces/hit-item';
+import { IHit } from './../../shared/interfaces/hit';
+import { IResponseServer } from './../../shared/interfaces/response-server';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class PostEffectService {
 
-  constructor() { }
+  constructor(private readonly _http: HttpClient) {}
+
+  public getByPage(page: number): Observable<IHit[]>{
+    return this._http.get<IResponseServer>('http://hn.algolia.com/api/v1/search_by_date?query=' + page).pipe(
+      map( response => {
+        return response['hits']
+      })
+    )
+  }
+
+  public getById(id: number): Observable<IHitItem>{
+    return this._http.get<IHitItem>('http://hn.algolia.com/api/v1/items/' + id)
+  }
 }
